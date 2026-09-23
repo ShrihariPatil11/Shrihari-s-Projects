2db49dcfe5adc0be0ebeda0ba2082ed507fa9524
Shrihari Nitin Patil <patilshrihari1109@gmail.com>
2026-09-23 10:19:51 +0530

    Add files via upload

---
+++ adult.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Enter your age \n");
+scanf("%d",&num);
+if(num>=18)
+printf("Person is Adult \n");
+else
+printf("Person is Minor \n");
+return 0;
+}
---
+++ diviby5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Enter your Number \n");
+scanf("%d", &num);
+if(num %5==0)
+printf(" %d is divisible by 5 \n",num);
+else
+printf(" %d is not divisible by 5 \n",num);
+return 0;
+}
